ba15e866974ec818ebf44e11e0ec9c32cc882b1e
WudDoo <89762267+WudDoo@users.noreply.github.com>
2022-12-15 13:53:38 +0000

    Add files via upload

--- Rank 1/Born2beRoot/monitoring.sh
+++ Rank 1/Born2beRoot/monitoring.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-echo  $'\t# Architecture:' `hostnamectl | grep "Operating System" | cut -d ' ' -f5-` \
-`arch` \
-$'\n\t# CPU(s): '`awk '/physical id/' /proc/cpuinfo | wc -l`  \
-$'\n\t# vCPU(s): '`awk '/processor/' /proc/cpuinfo | wc -l`
+arch=(`uname -svo`)
+pcpu=(`awk '/physical id/' /proc/cpuinfo | wc -l`)
+vcpu=(`awk '/processor/' /proc/cpuinfo | wc -l`)
+ram=(`free --mega | awk '/Mem/ {printf("%d/%dMB", $4, $2)}'`)
+ramp=(`free --mega | awk '/Mem/ {printf("%d%%", ($3/$2*100))}'`)
+wall "	@ Architecture: $arch
+	@ CPU(s): $pcpu
+	@ vCPU(s): $vcpu
+	@ Available RAM: $ram ($ramp in use)
+	@ Available Memory:
+"
