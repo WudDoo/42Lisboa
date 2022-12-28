8c8c3650621b163b805ab7ddc18c1447aa2038de
WudDoo <89762267+WudDoo@users.noreply.github.com>
2022-12-28 19:06:03 +0000

    big monitoring.sh update

--- Rank 1/Born2beRoot/monitoring.sh
+++ Rank 1/Born2beRoot/monitoring.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
-arch=(`uname -svo`)
-pcpu=(`awk '/physical id/' /proc/cpuinfo | wc -l`)
-vcpu=(`awk '/processor/' /proc/cpuinfo | wc -l`)
-ram=(`free --mega | awk '/Mem/ {printf("%d/%dMB", $4, $2)}'`)
-ramp=(`free --mega | awk '/Mem/ {printf("%.2f%%", ($3/$2*100))}'`)
-mem=(`df -H --total | awk '/total/ {printf("%d/%dGB", $4, $2)}'`)
-memp=(`df -H --total | awk '/total/ {printf("%.2f%%", ($3/$2*100))}'`)
-load=(`top -bn1 | awk '/^top/ {printf("%.2f%%", $12)}'`)
-date=(`who -b | awk '{print $3}'`)
-time=(`who -b | awk '{print $4}'`)
-lvm=(`lsblk | awk '/lvm/ {if ($1) {print "yes"} else {print "no"} }'`)
-echo "	#Architecture: $arch
-	#CPU(s): $pcpu
-	#vCPU(s): $vcpu
-	#RAM: $ram ($ramp usage)
-	#Disk Memory: $mem ($memp usage)
-	#CPU load: $load
-	#Last boot: $date $time
-	#LVM: $lvm
+arch="`uname -svo`"
+pcpu="`awk '/physical id/' /proc/cpuinfo | wc -l`"
+vcpu="`awk '/processor/' /proc/cpuinfo | wc -l`"
+ram="`free --mega | awk '/Mem/{printf("%d/%dMB (%.2f%% usage)", $4, $2, $3/$2*100)}'`"
+mem="`df -H --total | awk '/total/ {printf("%d/%dGB (%.2f%% usage)", $4, $2, $3/$2*100)}'`"
+load="`top -bn1 | awk '/^top/ {printf("%.2f%%", $(NF-2))}'`"
+bootdate="`who -b | awk '{print $3" "$4}'`"
+lvm=(`if [ "$(lsblk | grep lvm | wc -l)" -gt 0 ]; then printf "Yes"; else printf "No"; fi`)
+tcp="`ss | grep tcp | wc -l` Established"
+user="`who | cut -d ' ' -f 1 | sort -u | wc -l`"
+
+wall "  #Architecture: $arch
+        #CPU(s): $pcpu
+        #vCPU(s): $vcpu
+        #RAM: $ram
+        #Disk Memory: $mem
+        #CPU load: $load
+        #Last boot: $bootdate
+        #LVM: $lvm
+        #Connections TCP: $tcp
+        #User log: $user
 "
